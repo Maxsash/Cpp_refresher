89c13a51d2e245d8498ff99d894ee1ae67bf6f39
Maxsash <the.revived.1@gmail.com>
2019-07-18 02:55:52 +0530

    chore: Make a simple shape

--- main.cpp
+++ main.cpp
@@ -5,5 +5,15 @@ using namespace std;
 int main()
 {
   cout << "Hello world!" << endl;
+  //cout basically stands for 'console out'. Used for printing
+  //endl stands for 'end line'
+
+  //Printing a shape
+  cout << "    /|" << endl;
+  cout << "   / |" << endl;
+  cout << "  /  |" << endl;
+  cout << " /   |" << endl;
+  cout << "/____|" << endl;
+  
   return 0;
 }
