afd6e65a908d0ff8a9754c68ffb3d375eccc8de1
Maxsash <the.revived.1@gmail.com>
2019-07-27 17:58:00 +0530

    chore: Introduce variables

--- main.cpp
+++ main.cpp
@@ -8,12 +8,21 @@ int main()
   //cout basically stands for 'console out'. Used for printing
   //endl stands for 'end line'
 
+  //creating a variable
+  string character_name = "John";
+  int character_age;  //need not assign value straight away
+  character_age = 69; //No quotation marks here!
+
+  //using the variables
+  cout << "There once was a man named " << character_name << endl;
+  cout << "He was " <<character_age << " years old." << endl;
+
   //Printing a shape
   cout << "    /|" << endl;
   cout << "   / |" << endl;
   cout << "  /  |" << endl;
   cout << " /   |" << endl;
   cout << "/____|" << endl;
-  
+
   return 0;
 }
